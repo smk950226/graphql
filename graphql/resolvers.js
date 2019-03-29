2645cdc305f1230eef29b9a36c93cbe5860599a3
SEUNGMIN KIM <smk950226@naver.com>
2019-03-29 19:14:15 +0900

    query 고도화 1

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,11 +1,39 @@
-const smkim = {
-    name: "KimSeungMin",
-    age: 25,
-    gender: "male"
-}
+const people = [
+    {
+        name: "KimSeungMin",
+        age: 25,
+        gender: "male"
+    },
+    {
+        name: "John",
+        age: 20,
+        gender: "male"
+    },
+    {
+        name: "Harry",
+        age: 35,
+        gender: "female"
+    },
+    {
+        name: "Natasah",
+        age: 25,
+        gender: "female"
+    },
+    {
+        name: "Mellow",
+        age: 15,
+        gender: "male"
+    },
+    {
+        name: "Bong",
+        age: 25,
+        gender: "male"
+    }
+]
 
 const resolvers = {
     Query: {
+        people: () => people,
         person: () => smkim
     }
 }
