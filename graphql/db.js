b2a3b385cd85722d9f8d9b1e774e4201ab24c51c
SEUNGMIN KIM <smk950226@naver.com>
2019-03-29 23:06:25 +0900

    query 고도화 2

---
+++ graphql/db.js
@@ -0,0 +1,42 @@
+const people = [
+    {
+        id: "0",
+        name: "KimSeungMin",
+        age: 25,
+        gender: "male"
+    },
+    {
+        id: "1",
+        name: "John",
+        age: 20,
+        gender: "male"
+    },
+    {
+        id: "2",
+        name: "Harry",
+        age: 35,
+        gender: "female"
+    },
+    {
+        id: "3",
+        name: "Natasah",
+        age: 25,
+        gender: "female"
+    },
+    {
+        id: "4",
+        name: "Mellow",
+        age: 15,
+        gender: "male"
+    },
+    {
+        id: "5",
+        name: "Bong",
+        age: 25,
+        gender: "male"
+    }
+]
+
+export {
+    people
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,35 +1,4 @@
-const people = [
-    {
-        name: "KimSeungMin",
-        age: 25,
-        gender: "male"
-    },
-    {
-        name: "John",
-        age: 20,
-        gender: "male"
-    },
-    {
-        name: "Harry",
-        age: 35,
-        gender: "female"
-    },
-    {
-        name: "Natasah",
-        age: 25,
-        gender: "female"
-    },
-    {
-        name: "Mellow",
-        age: 15,
-        gender: "male"
-    },
-    {
-        name: "Bong",
-        age: 25,
-        gender: "male"
-    }
-]
+import { people } from './db';
 
 const resolvers = {
     Query: {
